0f414e92724fd620dd2794523cdf7bd37afbb18c
MaxenceGumiero <117424624+MaxouJS@users.noreply.github.com>
2023-02-22 15:54:21 +1100

    Update Bathroom.jsx

--- src/pages/Bathroom.jsx
+++ src/pages/Bathroom.jsx
@@ -35,7 +35,7 @@ const Loader = () => {
         <div className='flex-col text-center space-y-8'>
           <img src='./images/Tradelink.png' className='h-32' />
           <div className='flex-col space-y-4'>
-            <span className='text-4xl font-black text-white'>Loading {progress.toFixed(0)}%</span>
+            <span className='text-4xl font-black text-white animate-pulse'>Loading {progress.toFixed(0)}%</span>
           </div>
         </div>
       </div>
